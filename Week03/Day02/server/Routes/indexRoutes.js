2f4eaf2d237f7158419174a104c805b631ef1be9
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-09-29 11:23:13 +0700

    Create jobRoute.js and modify indexRoutes.js

--- Week03/Day02/server/Routes/indexRoutes.js
+++ Week03/Day02/server/Routes/indexRoutes.js
@@ -2,9 +2,12 @@ import RegRoute from'./regionRoute'
 import UsrRoute from './userRoute'
 import CtsRoute from './countryRoute'
 import LocRoute from './locationRoute'
+import JobRoute from './jobRoute'
+
 export default {
     RegRoute,
     UsrRoute,
     CtsRoute,
     LocRoute,
+    JobRoute,
 }
