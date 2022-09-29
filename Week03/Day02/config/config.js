dcf760f5d66d66d5fa5a854d35919d81ffdfcd7c
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-09-29 11:06:08 +0700

    Modify config.js with dotenv

--- Week03/Day02/config/config.js
+++ Week03/Day02/config/config.js
@@ -1,8 +1,11 @@
+
+import 'dotenv/config'
+
 const config = {
     env : process.env.NODE_ENV || 'dev',
-    port : 3000,
-    db_name : 'Batch#18',
-    db_username : 'postgres',
-    db_password : 'admin'
+    port : process.env.PGPORT,
+    db_name : process.env.PGDATABASE,
+    db_username : process.env.PGUSER,
+    db_password : process.env.PGPASSWORD
 }
 export default config
