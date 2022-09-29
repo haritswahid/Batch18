bb6a3c90585596b30899fbb85b959a479dae8d01
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-09-29 11:43:17 +0700

    Create employeeCtrl.js and modify indexCtrl.js

--- Week03/Day02/server/controller/indexCtrl.js
+++ Week03/Day02/server/controller/indexCtrl.js
@@ -3,10 +3,12 @@ import UsrCtrl from './userCtrl'
 import CtsCtrl from './countryCtrl'
 import LocCtrl from './locationCtrl'
 import JobCtrl from './jobCtrl'
+import EmpCtrl from './employeeCtrl'
 export default {
     RegCtrl,
     UsrCtrl,
     CtsCtrl,
     LocCtrl,
     JobCtrl,
+    EmpCtrl,
 }
