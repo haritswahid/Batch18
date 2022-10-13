84df2f1aa2d5298c365cdcb1bf5f6380a0fca6e9
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-10-14 03:31:51 +0700

    task menjadi api

--- Week04/Day02/src/app.module.ts
+++ Week04/Day02/src/app.module.ts
@@ -2,16 +2,13 @@
 
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiModule } from './api/api.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configService } from './config/config.service';
-import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
-    TasksModule,
-  ],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()), ApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
