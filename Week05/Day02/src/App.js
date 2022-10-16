df03807cf079a0e2ff474bf88802b988c9952902
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-10-16 16:26:48 +0700

    Membuat form dengan React

---
+++ Week05/Day02/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+// import Counter from './Counter/Counter';
+// import CounterArrow from './Counter/CounterArrow';
+// import CounterHook from './Counter/CounterHook';
+// import Parent from './ParentChild/Parent';
+// import EmployeeList from './List/EmployeeList';
+import ListEmployee from './Form/ListEmployee';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+      <ListEmployee/>
+    </div>
+  );
+}
+
+export default App;
