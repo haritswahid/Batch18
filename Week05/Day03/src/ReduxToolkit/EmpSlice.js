df9984c31772f620dc95df86fec6f159c350e15c
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-10-18 22:49:55 +0700

    Memperbaiki kode dan menambahkan method untuk mengubah salary

--- Week05/Day03/src/ReduxToolkit/EmpSlice.js
+++ Week05/Day03/src/ReduxToolkit/EmpSlice.js
@@ -17,15 +17,33 @@ export const empSlice = createSlice({
             return state
         },
         doAddEmps: (state, action) =>{
-            const {payload} = action
+            const { payload } = action
+            const final_payload = {...payload, salary: parseInt(payload.salary)}
+            const up_employee = [...state.employee,final_payload];
             return {
                 ...state,
-                employee : [...state.employee,payload],
-                totalSalary : listOfEmployee.reduce((sum,el)=> sum + el.salary,0)
+                employee : up_employee,
+                totalSalary : up_employee.reduce((sum,el)=> sum + el.salary,0)
+            }
+        },
+        doChangeSalary: (state, action) =>{
+            const { payload } = action;
+            const up_employee = state.employee.map(d =>{ 
+                if(d.empId === payload.empId) return {
+                        ...d,
+                        salary: d.salary + payload.num
+                    }
+                return d
+            });
+            
+            return {
+                ...state,
+                employee: up_employee,
+                totalSalary : up_employee.reduce((sum,el)=> sum + el.salary,0)
             }
         }
     }
 })
 
-export const {doAddEmps,doGetEmps} = empSlice.actions
+export const {doAddEmps,doGetEmps,doChangeSalary} = empSlice.actions
 export default empSlice.reducer
