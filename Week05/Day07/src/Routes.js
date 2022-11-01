54127c4545772859c16b3d00dd622509b60381ec
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-11-01 09:36:06 +0700

    Menambahkan materi React dengan Tailwind CSS

---
+++ Week05/Day07/src/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Navigate,useRoutes} from 'react-router-dom'
+import Dashboard from './Layout/Dashboard'
+import RegionViewApi from './ViewApi/RegionViewApi'
+import FormikRegionViewApi from './ViewApi/FormikRegionViewApi'
+import FormikRegionViewApiRedux from './ViewReduxSaga/FormikRegionViewApi'
+
+export default function Routes() {
+  return useRoutes([
+    {
+        path: '/',
+        element :<Dashboard/>,
+        children:[
+            {path:'region', element:<RegionViewApi/>},
+            {path:'regionformik', element:<FormikRegionViewApi/>},
+            {path:'regionredux',element:<FormikRegionViewApiRedux/>}
+        ]
+    },
+    {
+        path:'*', element:<Navigate to='/404' replace/>
+    }
+  ])
+}
