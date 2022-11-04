c581e125c2a1fe62ae935d652784a228b872cc0e
Harits Wahid Vijayanto <haritswahid99@gmail.com>
2022-11-04 08:20:35 +0700

    Inisiasi Week06

---
+++ Week06/Day02/pages/config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    domain: 'http://localhost:3002',
+    urlImg: 'http://localhost:3002/api/region/files',
+    image: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '3002',
+                pathname: '/api/region/files/**',
+            },
+        ],
+    }
+}
